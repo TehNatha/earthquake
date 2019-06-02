437dafa0230b701add32004f87f48873ee012cfe
TehNatha <nathan.stier@gmail.com>
2019-06-02 15:38:52 -0700

    Display quake ids on mainactivity

--- app/src/main/java/com/tehnatha/earthquake/MainActivity.java
+++ app/src/main/java/com/tehnatha/earthquake/MainActivity.java
@@ -1,17 +1,34 @@
 package com.tehnatha.earthquake;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView v = new TextView(this);
-        v.setText(BuildConfig.END_POINT);
+        EarthquakeRepository repo = new EarthquakeRepository(getApplication());
+        LiveData<List<Earthquake>> earthquakes = repo.getAllEarthQuakes();
+        final TextView v = new TextView(this);
+        earthquakes.observe(this, new Observer<List<Earthquake>>() {
+            @Override
+            public void onChanged(@Nullable List<Earthquake> earthquakes) {
+                StringBuilder builder = new StringBuilder();
+                if (earthquakes == null) builder.append("failed");
+                else
+                    for (Earthquake quake : earthquakes)
+                        builder.append(quake.getEqid()).append("\n");
+                v.setText(builder.toString());
+            }
+        });
         setContentView(v);
     }
 }
